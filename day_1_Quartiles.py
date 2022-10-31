9c7232f30751512f8380148f897c3329540806ed
Kaushik Manjunatha <38116333+kaushikmupadhya@users.noreply.github.com>
2022-10-31 19:48:59 +0100

    Update day_1_Quartiles.py

--- day_1_Quartiles.py
+++ day_1_Quartiles.py
@@ -13,8 +13,29 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def quartiles(arr):
+def quartiles(arr, level):
     # Write your code here
+    arr = sorted(arr) if level==0 else arr
+    odd = True if len(arr)%2!=0 else False
+    idx_q2 = ((len(arr)+1)/2)-1
+    res = []
+    level+=1
+    if level==2:
+        if odd:
+            return int(arr[int(idx_q2)])
+        else:
+            return int((arr[int(idx_q2)]+arr[int(math.ceil(idx_q2))])/2)
+    else:
+        if odd:
+            res.append(quartiles(arr[:int(idx_q2)], level))
+            res.append(arr[int(idx_q2)])
+            res.append(quartiles(arr[int(idx_q2+1):], level))
+        else:
+            res.append(quartiles(arr[:int(math.ceil(idx_q2))], level))
+            res.append(quartiles(arr, level))
+            res.append(quartiles(arr[int(math.ceil(idx_q2)):], level))
+    return res
+    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -23,7 +44,7 @@ if __name__ == '__main__':
 
     data = list(map(int, input().rstrip().split()))
 
-    res = quartiles(data)
+    res = quartiles(data, 0)
 
     fptr.write('\n'.join(map(str, res)))
     fptr.write('\n')
